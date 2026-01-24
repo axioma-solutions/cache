2d82f1430f3195da762de20ab30aa646cec9d1bc
Benedek Horváth <benedekhorvath@edu.bme.hu>
2026-01-24 13:01:34 +0100

    add sprinting

--- player.gd
+++ player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
 const SPEED = 5.0
+const SPRINT_SPEED = 10.0
 const JUMP_VELOCITY = 4.5
-const MOUSE_SENSITIVITY = 0.003
+const MOUSE_SENSITIVITY = 0.002
+const AIR_ACCELERATION = 2.0
 
 @onready var camera = $Camera3D
 
@@ -26,13 +28,19 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	var current_speed = SPEED
+	
+	if Input.is_action_pressed("sprint"):
+		current_speed = SPRINT_SPEED
+	
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
